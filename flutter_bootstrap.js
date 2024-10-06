dd5a7ce3d7b122fe5f3ec5d4f079383cf97f089c
AlviNabil <alvi.nabil@bkash.com>
2024-10-06 12:22:12 +0600

    Initial Commit 2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "929805181"
+    serviceWorkerVersion: "1286396921"
   }
 });
